b426f868d42bddaba4fa41323ea56fec6fc01368
BLecour <bigbenlecour@gmail.com>
2023-04-25 15:21:59 -0400

    changed widget spacing

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinter import *
 import sv_ttk
 from yt_dlp import YoutubeDL
+from time import sleep
 
 def main():
 
@@ -26,6 +27,7 @@ def main():
             progressBar.update()
 
     def downloadVideo():
+        statusText.config(text="")
         progressBar.pack(anchor="center")
         url = inputBox.get("1.0", "end-1c")
         ydl_opts = {
@@ -35,31 +37,37 @@ def main():
         if audioToggle.get():
             ydl_opts.update({"format": "bestaudio[ext=m4a]"})
 
-        with YoutubeDL(ydl_opts) as ydl:
-            ydl.download(url)
+        try:
+            with YoutubeDL(ydl_opts) as ydl:
+                ydl.download(url)
+        except: 
+            statusText.config(text=f"URL is not valid.")
 
     def optionsWindow():
         clearScreen()
         audio = ttk.Checkbutton(root, text="Audio Only", variable=audioToggle, onvalue=1, offvalue=0)
-        audio.pack()
-        backButton = ttk.Button(root, text="Back to Main", command=mainWindow)
-        backButton.pack()
+        audio.pack(pady=20)
+        backButton = ttk.Button(root, text="Back to Main", command=lambda: [clearScreen(), mainWindow()])
+        backButton.pack(pady=20)
 
     def clearScreen():
         for widget in root.winfo_children():
             widget.pack_forget()
+        root.update()
 
-    inputBox = tk.Text(root, height=10, width=50)
-    inputBox.config(borderwidth=2, relief='groove')
+    inputBox = tk.Text(root, height=10, width=50, borderwidth=2, relief='groove')
     downloadButton = ttk.Button(root, text="Download", command=downloadVideo)
     optionsButton = ttk.Button(root, text="Options", command=optionsWindow)
     progressBar = ttk.Progressbar(root, length=300)
+    statusText = ttk.Label(root)
 
     def mainWindow():
         clearScreen()
-        inputBox.pack(anchor='center')
-        downloadButton.pack(anchor='center')
-        optionsButton.pack(anchor='center')
+        inputBox.pack(anchor='center', pady=20)
+        downloadButton.pack(anchor='center', pady=10)
+        optionsButton.pack(anchor='center', pady=10)
+        statusText.pack(anchor='center', pady=20)
+        statusText.config(text="")
 
     mainWindow()
 
