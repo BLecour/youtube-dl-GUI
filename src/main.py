43717250521212e02f5c8f140d42ee8d6c8058a8
Ben <63076037+bentheball1@users.noreply.github.com>
2023-04-28 23:54:49 -0400

    added download ETA and thumbnail display

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
-import sv_ttk
 from yt_dlp import YoutubeDL
+from PIL import Image, ImageTk
+from urllib.request import urlopen
+import sv_ttk
 
 def main():
 
@@ -17,6 +19,7 @@ def main():
     sv_ttk.set_theme("dark")
 
     url = ""
+    thumbnailURL = ""
 
     def progress_hook(d):
         global videoTitle
@@ -25,12 +28,19 @@ def main():
                 percent = round(float(d['downloaded_bytes'] / d['total_bytes'] * 100), 2)
             else:
                 percent = round(float(d['downloaded_bytes'] / d['total_bytes_estimate'] * 100), 2)
-            progressText.config(text=f"{percent}% of {d['_total_bytes_str']} at {d['_speed_str']}")
+
+            # convert bytes per second to mebibytes per second
+            speed = round(float(d['speed']) / 1048576.0, 2) 
+            seconds = int(d['eta']) % 60
+            minutes = int(d['eta'] / 60)
+            progressText.config(text=f"{percent}% of {d['_total_bytes_str']} at {speed}MiB/s\nETA: {minutes:02d}:{seconds:02d}")
+
             progressBar['value'] = percent
             progressBar.update()
             statusText.config(text=f'Downloading: "{videoTitle}"')
         elif d['status'] == 'finished':
-            progressText.config(text="100.0%")
+            speed = round(float(d['speed']) / 1048576.0, 2) 
+            progressText.config(text=f"100.00% of {d['_total_bytes_str']} at {speed}MiB/s\nETA: 00:00")
             progressBar['value'] = 100.0
             statusText.config(text=f'Finished: "{videoTitle}"')
 
@@ -52,6 +62,19 @@ def main():
                 global videoTitle
                 videoInfo = ydl.extract_info(url, download=False)
                 videoTitle = videoInfo.get('title', None)
+                try:
+                    thumbnailURL = "http://img.youtube.com/vi/" + videoInfo.get("id", None) + "/0.jpg"
+                    print(thumbnailURL)
+                    u = urlopen(thumbnailURL)
+                    thumbnailData = u.read()
+                    u.close()
+                    thumbnail = ImageTk.PhotoImage(data=thumbnailData)
+                    thumbnailLabel = ttk.Label(image=thumbnail)
+                    thumbnailLabel.image = thumbnail
+                    thumbnailLabel.pack()
+
+                except Exception as e:
+                    print(f"Unable to display thumbnail: {e}")
                 ydl.download(url)
         except Exception as e:
             if " is not a valid URL." in str(e):
